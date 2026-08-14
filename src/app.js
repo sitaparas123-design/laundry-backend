679ad530db3413410e9d3c06e8ec48c5f2a01773
Sourabh Yadav <syadavv8640@gmail.com>
2026-08-14 12:23:30 +0530

    pst

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 
 // Import routes
 const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth');
+
 const branchRoutes = require('./routes/branches');
 const serviceRoutes = require('./routes/services');
 const staffRoutes = require('./routes/staff');
